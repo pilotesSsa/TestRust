a7e699f001e58d083404987d6c3d022f2ce6d826
Evgeniya Shalnova <e.shalnova@smartengines.com>
2025-05-13 19:29:03 +0300

    enh: example "Guess the number"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Угадайте номер!");
+
+    println!("Пожалуйста, введите свое предположение.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("Вы угадали: {guess}");
 }
