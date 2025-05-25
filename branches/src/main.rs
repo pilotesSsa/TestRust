df565359381dbe921542cc886fe2b7fe274edd12
Evgeniya Shalnova <e.shalnova@smartengines.com>
2025-05-25 17:28:24 +0300

    enh: add examples

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -6,4 +6,24 @@ fn main() {
     } else {
         println!("condition was false");
     }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let number = 6;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
 }
